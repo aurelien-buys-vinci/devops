9c48721fe7c7ef7d34cbb2d9f48aed3c221898a6
TemsamaniJawad <Jawad.temsamani@student.vinci.be>
2024-11-13 16:07:30 +0100

    Delete: exoplanete

--- exoplanetes/0-exoplanets_web1_template/models/Exolune.js
+++
@@ -1,9 +0,0 @@
-const exolunes = [];
-const DH = {name : "DH Tauri"};
-const Kelper = {name : "Kelper-409"};
-const WASP = {name : "WASP-49"};
-exolunes.push(DH,Kelper,WASP);
-
-module.exports.exolunes = () => {
-    return exolunes;
-};
