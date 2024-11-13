e8b27dfc57a198da412830efe4a2cca079016808
buys <aurelien.buys@student.vinci.be>
2024-11-13 22:02:51 +0100

    add: when 4 hour, return empty string

--- berlin-clock-kata/spec/appSpec.js
+++ berlin-clock-kata/spec/appSpec.js
@@ -76,4 +76,9 @@ describe('list of test for blockOfFiveHours function', function() {
         const resultat = main.blockOfFiveHours(1);
         expect(resultat).toBe("");
     });
+
+    it('should return "" for 4 hours', function() {
+        const resultat = main.blockOfFiveHours(4);
+        expect(resultat).toBe("");
+    });
 });    
