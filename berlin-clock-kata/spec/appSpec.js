7a2c468c922dd71ba9322ce22d0ffb8f4f0651ca
buys <aurelien.buys@student.vinci.be>
2024-11-13 16:40:47 +0100

    add: class main

--- berlin-clock-kata/spec/appSpec.js
+++ berlin-clock-kata/spec/appSpec.js
@@ -1,7 +1,1 @@
-const berlinClock = require('../app');
-
-describe("Berlin Clock", () => {
-  it("should have a function to convert time to Berlin Clock format", () => {
-    expect(typeof berlinClock.convertTime).toBe("function");
-  });
-});
+const Main = require('../app.js');
