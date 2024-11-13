d5df0daf057892ff448e586e7e99a0f1dde1a956
buys <aurelien.buys@student.vinci.be>
2024-11-13 19:37:34 +0100

    refactor: simpleHours function

--- berlin-clock-kata/app.js
+++ berlin-clock-kata/app.js
@@ -1,14 +1,9 @@
 export class Main {
     simpleHours(hours) {
-        if (hours === 1) {
-            return "R";
+        let result = "";
+        for (let i = 0; i < hours%5; i++) {
+            result += "R";
         }
-        if (hours === 2) {
-            return "RR";
-        }
-        if (hours === 4) {
-            return "RRRR";
-        }
-        return "";
+        return result;
     };
 }
