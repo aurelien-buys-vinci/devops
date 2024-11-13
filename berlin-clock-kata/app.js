13d39beb978895d00deaf34f006a61beefa91c7b
buys <aurelien.buys@student.vinci.be>
2024-11-13 19:30:46 +0100

    add: when given 1 hour, return R

--- berlin-clock-kata/app.js
+++ berlin-clock-kata/app.js
@@ -1,5 +1,8 @@
 export class Main {
     simpleHours(hours) {
+        if (hours === 1) {
+            return "R";
+        }
         return "";
     };
 }
--- berlin-clock-kata/spec/appSpec.js
+++ berlin-clock-kata/spec/appSpec.js
@@ -7,4 +7,9 @@ describe('Main', function() {
         const resultat = main.simpleHours();
         expect(resultat).toBe("");
     });
+
+    it('should return "R" for 1 hours', function() {
+        const resultat = main.simpleHours(1);
+        expect(resultat).toBe("R");
+    });
 });
